431cdfc02e830aec606ff1870e8217a54d18dbbd
airabino <aaron.i.rabinowitz@gmail.com>
2023-09-25 17:35:16 -0700

    Aaron 9/25/2023 1: Completed documentation

--- src/optimization.py
+++ src/optimization.py
@@ -13,36 +13,6 @@ from scipy.stats import binom
 
 from .utilities import IsIterable,FullFact
 
-def Solution(model):
-	'''
-	From StackOverflow
-	https://stackoverflow.com/questions/67491499/
-	how-to-extract-indexed-variable-information-in-pyomo-model-and-build-pandas-data
-	'''
-	model_vars=model.component_map(ctype=pyomo.Var)
-
-	serieses=[]   # collection to hold the converted "serieses"
-	for k in model_vars.keys():   # this is a map of {name:pyo.Var}
-		v=model_vars[k]
-
-		# make a pd.Series from each    
-		s=pd.Series(v.extract_values(),index=v.extract_values().keys())
-
-		# if the series is multi-indexed we need to unstack it...
-		if type(s.index[0])==tuple:# it is multi-indexed
-			s=s.unstack(level=1)
-		else:
-			s=pd.DataFrame(s) # force transition from Series -> df
-
-		# multi-index the columns
-		s.columns=pd.MultiIndex.from_tuples([(k,t) for t in s.columns])
-
-		serieses.append(s)
-
-	df=pd.concat(serieses,axis=1)
-
-	return df
-
 class CHRP():
 
 	def __init__(self,inputs={}):
@@ -58,7 +28,7 @@ class CHRP():
 		solver=pyomo.SolverFactory(**solver_kwargs)
 		solver.solve(self.model)
 
-		self.solution=Solution(self.model)
+		self.solution=self.Solution()
 
 	def Build(self):
 
@@ -179,6 +149,34 @@ class CHRP():
 					expr=(self.inputs['f_p_max'][r]*self.model.u_pc[t,r]-
 						self.model.u_p[t,r]>=0))
 
+	def Solution(self):
+		'''
+		From StackOverflow
+		https://stackoverflow.com/questions/67491499/
+		how-to-extract-indexed-variable-information-in-pyomo-model-and-build-pandas-data
+		'''
+		model_vars=self.model.component_map(ctype=pyomo.Var)
+
+		serieses=[]   # collection to hold the converted "serieses"
+		for k in model_vars.keys():   # this is a map of {name:pyo.Var}
+			v=model_vars[k]
+
+			# make a pd.Series from each    
+			s=pd.Series(v.extract_values(),index=v.extract_values().keys())
+
+			# if the series is multi-indexed we need to unstack it...
+			if type(s.index[0])==tuple:# it is multi-indexed
+				s=s.unstack(level=1)
+			else:
+				s=pd.DataFrame(s) # force transition from Series -> df
+
+			# multi-index the columns
+			s.columns=pd.MultiIndex.from_tuples([(k,t) for t in s.columns])
+
+			serieses.append(s)
+
+		self.solution=pd.concat(serieses,axis=1)
+
 class SCHRP():
 
 	def __init__(self,inputs={}):
@@ -194,7 +192,7 @@ class SCHRP():
 		solver=pyomo.SolverFactory(**solver_kwargs)
 		solver.solve(self.model)
 
-		# self.solution=Solution(self.model)
+		self.solution=self.Solution()
 
 	def Build(self):
 
@@ -351,3 +349,34 @@ class SCHRP():
 					self.model.unit_commitment.add(
 						expr=(self.inputs['f_p_max'][r]*self.model.u_pc[t,r,s]-
 							self.model.u_p[t,r,s]>=0))
+
+	def Solution(self):
+		'''
+		From StackOverflow
+		https://stackoverflow.com/questions/67491499/
+		how-to-extract-indexed-variable-information-in-pyomo-model-and-build-pandas-data
+		'''
+		model_vars=self.model.component_map(ctype=pyomo.Var)
+
+		serieses=[]   # collection to hold the converted "serieses"
+		for k in model_vars.keys():   # this is a map of {name:pyo.Var}
+			v=model_vars[k]
+
+			# make a pd.Series from each    
+			s=pd.Series(v.extract_values(),index=v.extract_values().keys())
+
+			# if the series is multi-indexed we need to unstack it...
+			if type(s.index[0])==tuple:# it is multi-indexed
+				if len(s.index[0])==3:
+					s=s.unstack(level=(1,2))
+				elif len(s.index[0])==2:
+					s=s.unstack(level=1)
+			else:
+				s=pd.DataFrame(s) # force transition from Series -> df
+
+			# multi-index the columns
+			s.columns=pd.MultiIndex.from_tuples([(k,t) for t in s.columns])
+
+			serieses.append(s)
+
+		self.solution=pd.concat(serieses,axis=1)
